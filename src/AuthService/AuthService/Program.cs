075a4817371f137f6b88357f86694394557524ef
Chris Farmer <chrisfarmer19801@outlook.com>
2023-01-28 12:32:58 +0000

    Setup basic ids

---
+++ src/AuthService/AuthService/Program.cs
@@ -0,0 +1,11 @@
+﻿using AuthService.IdentityServer;
+
+var appBuilder = WebApplication.CreateBuilder(args);
+appBuilder.Services.AddIdentityServer()
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients);
+
+var webApp = appBuilder.Build();
+webApp.UseIdentityServer();
+
+webApp.Run("https://*:5001");
